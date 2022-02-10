ccc03d1ace25aa9d1926419a22553d668af59861
Your Name <you@example.com>
2022-02-10 22:19:35 +0530

    access the valu

---
+++ restAPIProject6/src/main/java/restAPIProject6/accessmodifier.java
@@ -0,0 +1,8 @@
+package restAPIProject6;
+
+public class accessmodifier {
+   
+   int addTwoNumbers(int a, int b){
+	return a+b;
+   
+   }}
---
+++ restAPIProject6/src/main/java/restAPIProject6/accessmodifier2.java
@@ -0,0 +1,7 @@
+package restAPIProject6;
+class Test extends accessmodifier{
+   public static void main(String args[]){
+	Test obj = new Test();
+	System.out.println(obj.addTwoNumbers(11, 22));
+}
+}
